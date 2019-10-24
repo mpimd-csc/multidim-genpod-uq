2887ba33f676ee6d072e0d57ea3eefd6b7c56e7d
highlando <jnhlnd@gmail.com>
2019-10-24 23:53:27 +0800

    can now solve a single prob

--- scripts/various_diffusion_coeffs.py
+++ scripts/various_diffusion_coeffs.py
@@ -1,4 +1,4 @@
-import numpy as np
+# import numpy as np
 import scipy.sparse as sps
 import scipy.sparse.linalg as spsla
 import matplotlib.pyplot as plt
@@ -54,29 +54,51 @@ ininds = bcinidx['ininds']
 lplclst = []
 kk = 1
 nv = len(ininds)
-amat = sps.csr_matrix((nv, nv))
+
 for kk in range(Nrgs):
-    knu = nulist[kk]
-    akform = dolfin.assemble((knu*dolfin.inner(dolfin.grad(u),
+    # assemble all mit nu=1
+    akform = dolfin.assemble((1.*dolfin.inner(dolfin.grad(u),
                              dolfin.grad(v)))*dx(kk))
     Akmat = dts.mat_dolfin2sparse(akform)
     Akmat.eliminate_zeros()
     Akmat, _ = dts.condense_velmatsbybcs(Akmat, invinds=ininds,
                                          dbcinds=bcinds, dbcvals=bcvals)
-    print(kk, Akmat.nnz, np.linalg.norm(Akmat.data))
     lplclst.append(Akmat)
-    amat = amat + Akmat
 
-onedolfun = dolfin.Constant('1')
-testplotfile = dolfin.File('checkit.pvd')
-for kk in range(Nrgs):
-    onedolfvec = dolfin.assemble(v*onedolfun*dx(kk))
-    monevec = ((onedolfvec.get_local())[ininds].reshape((nv, 1)))
-    onevec = spsla.spsolve(mmat, monevec).reshape((nv, 1))
-    aone = lplclst[kk]*onevec
-    print(kk, np.linalg.norm(aone))
-    aonefun = dts.expand_vecnbc_dolfunc(V=V, vec=aone, invinds=ininds,
-                                        bcindsl=[bcinds], bcvalsl=[bcvals])
-    # dolfin.plot(aonefun)
-    # plt.show()
-    testplotfile << aonefun, kk
+
+def _realize_amat(nulist):
+    amat = sps.csr_matrix((nv, nv))
+    for kk, knu in enumerate(nulist):
+        amat = amat + knu*lplclst[kk]
+    return amat
+
+# onedolfun = dolfin.Constant('1')
+# testplotfile = dolfin.File('checkit.pvd')
+# for kk in range(Nrgs):
+#     onedolfvec = dolfin.assemble(v*onedolfun*dx(kk))
+#     monevec = ((onedolfvec.get_local())[ininds].reshape((nv, 1)))
+#     onevec = spsla.spsolve(mmat, monevec).reshape((nv, 1))
+#     aone = lplclst[kk]*onevec
+#     print(kk, np.linalg.norm(aone))
+#     aonefun = dts.expand_vecnbc_dolfunc(V=V, vec=aone, invinds=ininds,
+#                                         bcindsl=[bcinds], bcvalsl=[bcvals])
+#     # dolfin.plot(aonefun)
+#     # plt.show()
+#     testplotfile << aonefun, kk
+
+# rhs = dolfin.Expression("sin(3.14159*(x[1]*x[1]+[x[1]*x[1]))", degree=1)
+rhsexp = dolfin.\
+    Expression("sin(2*pi*(pow(x[0],2)+pow(x[1],2)))*sin(pi*4*x[0])", degree=1)
+
+rhs = dolfin.assemble(v*rhsexp*dolfin.dx)
+rhsvec = rhs.get_local()[ininds]
+
+amat = _realize_amat(nulist)
+solvec = spsla.spsolve(amat, rhsvec).reshape((nv, 1))
+solv = dts.expand_dolfunc(solvec, bcinds=bcinds, bcvals=bcvals,
+                          ininds=ininds, V=V)
+
+# rhs = dolfin.Expression("- 10*exp(- pow(x[1] - 0.5, 2))", degree=1)
+# rhsfun = dolfin.interpolate(rhsexp, V)
+dolfin.plot(solv)
+plt.show()
