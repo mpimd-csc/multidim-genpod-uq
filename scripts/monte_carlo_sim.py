46cd97c50bf1e4ecd1feec0718590a426b7d63ff
highlando <jnhlnd@gmail.com>
2019-10-25 22:25:41 +0800

    mc vs pce

--- scripts/monte_carlo_sim.py
+++ scripts/monte_carlo_sim.py
@@ -1,43 +1,74 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import spacetime_galerkin_pod.chaos_expansion_utils as ceu
+
 from circle_subsec import get_problem
 
 get_linop, get_sol, get_output, problemfems = get_problem()
 
 basenu = 1e-3
+varia = 0.
+varib = 1e-4
 
-Nruns = 100
+mcits, mcruns = 25, 1000  # 200
+pcedimlist = [3, 5, 8, 12, 17]
+
+plotplease = False
+
+# ## CHAP Monte Carlo
 
 
 def realizenulist(poslist):
     nulist = [basenu]*5
     for k in poslist:
-        nulist[k] = basenu + ((1e-4*np.random.rand(1)).flatten())[0]
+        nulist[k] = basenu + (((varib-varia)*np.random.rand(1)).flatten())[0]
     return nulist
 
-estxnu, estxy = 0, 0
+# estxnu, estxy = 0, 0
 ylist, xnulist = [], []
-for mck in range(Nruns):
-    nulist = realizenulist([4])
-    estxnu += nulist[4]
-    cury = get_output(nulist, plotfignum=None)
-    ylist.append(cury)
-    xnulist.append(nulist[4])
-    estxy += cury
+for mitk in range(mcits):
+    for mck in range(mcruns):
+        nulist = realizenulist([4])
+        # estxnu += nulist[4]
+        cury = get_output(nulist, plotfignum=None)
+        ylist.append(cury)
+        xnulist.append(nulist[4])
+        # estxy += cury
+    estxnu = np.average(np.array(nulist))
+    estxy = np.average(np.array(ylist))
 
-estxnu = 1./Nruns*estxnu
-estxy = 1./Nruns*estxy
-
-print('estxnu={0}, estxy={1}'.format(estxnu, estxy))
+    print('mc:{0}/{1}: estxy={2}'.format((mitk+1)*mcruns, mcits*mcruns, estxy))
 
 nulist = [basenu]*5
-nulist[4] = estxnu
+nulist[4] = basenu+.5*(varib-varia)  # estxnu
 cury = get_output(nulist, plotfignum=None)
 print('y(estxnu)={0}'.format(cury))
 
-plt.figure(89)
-plt.plot(ylist, '.')
-plt.figure(98)
-plt.plot(xnulist, '.')
-plt.show()
+if plotplease:
+    plt.figure(89)
+    plt.plot(ylist, '.')
+    plt.figure(98)
+    plt.plot(xnulist, '.')
+    plt.show()
+
+
+# ## CHAP Polynomial Chaos Expansion
+
+nua, nub = basenu+varia, basenu+varib
+
+for pcedim in pcedimlist:
+    abscissae, weights = ceu.get_gaussqr_uniform(N=pcedim, a=nua, b=nub)
+
+    nulist = [basenu]*5
+    ylist = []
+    for cnu in abscissae:
+        nulist[4] = cnu
+        ylist.append(get_output(nulist, plotfignum=None))
+
+    exypce = 0
+    for kk, cw in enumerate(weights):
+        exypce += cw*ylist[kk]
+
+    print('pcedim={0:2.0f}, exypce={1}'.format(pcedim,
+                                               1./(varib-varia)*exypce))
