e46b88ea8d39608bb9ba8acd7333ac91f3363d6c
highlando <jnhlnd@gmail.com>
2019-11-01 10:06:39 +0100

    looks like good -- gonna do a refac

--- scripts/circle_subsec.py
+++ scripts/circle_subsec.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.sparse as sps
 import scipy.sparse.linalg as spsla
+import numpy.linalg as npla
 import matplotlib.pyplot as plt
 
 import dolfin
@@ -12,6 +13,22 @@ meshprfx = '../mesh/5-segs_lvl1'
 meshfile = meshprfx + '.xml.gz'
 physregs = meshprfx + '_physical_region.xml.gz'
 fctsregs = meshprfx + '_facet_region.xml.gz'
+
+
+class CircObsvDomain(dolfin.SubDomain):
+
+    def __init__(self, cx=0., cy=0., radius=.1):
+        dolfin.SubDomain.__init__(self)
+        self.cx = cx
+        self.cy = cy
+        self.rsqrd = radius**2
+
+    def inside(self, x, on_boundary):
+        xshftd = x[0]-self.cx
+        yshftd = x[1]-self.cy
+        insi = yshftd**2 + xshftd**2 < self.rsqrd
+        # print(x, insi)
+        return insi
 
 
 def get_problem():
@@ -42,6 +59,15 @@ def get_problem():
     lplclst = []
     kk = 1
     nv = len(ininds)
+    convfac = 1.
+
+    convexp = dolfin.Expression(('(x[0]*x[0]+x[1]*x[1]-1)*x[1]',
+                                 '(1-x[0]*x[0]-x[1]*x[1])*x[0]'), degree=1)
+    convform = dolfin.assemble(v*dolfin.inner(convexp,
+                               dolfin.grad(u))*dolfin.dx)
+    convmat = convfac*dts.mat_dolfin2sparse(convform)
+    convmat, _ = dts.condense_velmatsbybcs(convmat, invinds=ininds,
+                                           dbcinds=bcinds, dbcvals=bcvals)
 
     for kk in range(Nrgs):
         # assemble all mit nu=1
@@ -57,7 +83,7 @@ def get_problem():
         amat = sps.csr_matrix((nv, nv))
         for kk, knu in enumerate(nulist):
             amat = amat + knu*lplclst[kk]
-        return amat
+        return amat + convmat
 
     examplerhsexp = dolfin.\
         Expression("sin(2*pi*(pow(x[0],2)+pow(x[1],2)))*sin(pi*4*x[0])",
@@ -65,14 +91,19 @@ def get_problem():
     rhs = dolfin.assemble(v*examplerhsexp*dx(0)+v*examplerhsexp*dx(2))
     examplerhsvec = rhs.get_local()[ininds]
 
-    def realize_sol(nulist, rhs=None):
-        rhsvec = examplerhsvec if rhs is None else rhs
-        amat = realize_linop(nulist)
-        solvec = spsla.spsolve(amat, rhsvec).reshape((nv, 1))
-        return solvec
-
-    problemfems = dict(mmat=mmat,
-                       bcinds=bcinds, bcvals=bcvals, ininds=ininds)
+    def realize_sol(nulist, rhs=None, podmat=None):
+        if podmat is None:
+            rhsvec = examplerhsvec if rhs is None else rhs
+            amat = realize_linop(nulist)
+            solvec = spsla.spsolve(amat, rhsvec).reshape((nv, 1))
+            return solvec
+        else:
+            knv = podmat.shape[1]
+            rhsvec = podmat.T.dot(examplerhsvec) if rhs is None else rhs
+            amat = realize_linop(nulist)
+            amat = podmat.T.dot(amat*podmat)
+            solvec = npla.solve(amat, rhsvec).reshape((knv, 1))
+            return podmat.dot(solvec)
 
     def plotit(vvec=None, vfun=None, fignum=1):
         if vfun is None:
@@ -82,17 +113,37 @@ def get_problem():
         dolfin.plot(vfun)
         plt.show()
 
-    def realize_output(nulist, rhs=None, plotfignum=None, pointeva=False):
-        solvec = realize_sol(nulist, rhs=rhs)
-        solv = dts.expand_dolfunc(solvec, bcinds=bcinds, bcvals=bcvals,
-                                  ininds=ininds, V=V)
-        if plotfignum is not None:
-            plotit(vfun=solv, fignum=plotfignum)
-        if pointeva:
-            midpt = dolfin.Point(0, 0)
-            output = solv(midpt)
+    # ## Output
+
+    obsvr = .1
+    obsarea = np.pi*obsvr**2
+    obsvdom = CircObsvDomain(cx=.0, cy=0., radius=obsvr)
+    cellmarkers = dolfin.MeshFunction('size_t', V.mesh(),
+                                      V.mesh().topology().dim())
+    obsvdom.mark(cellmarkers, 111)  # just 0 didnt work
+    dxobs = dolfin.Measure('dx', subdomain_data=cellmarkers)
+    cform = dolfin.assemble(1./obsarea*u*dxobs(111)).\
+        get_local().reshape((1, V.dim()))
+    cmat = sps.csc_matrix(cform)[:, ininds]
+
+    def realize_output(nulist, rhs=None, plotfignum=None,
+                       pointeva=False, podmat=None):
+        solvec = realize_sol(nulist, rhs=rhs, podmat=podmat)
+        if plotfignum is not None or pointeva:
+            solv = dts.expand_dolfunc(solvec, bcinds=bcinds, bcvals=bcvals,
+                                      ininds=ininds, V=V)
+            if plotfignum is not None:
+                plotit(vfun=solv, fignum=plotfignum)
+                output = cmat.dot(solvec)
+            if pointeva:
+                midpt = dolfin.Point(0, 0)
+                output = solv(midpt)
         else:
-            output = np.sqrt(dolfin.assemble(solv*solv*dx(4)))
+            output = cmat.dot(solvec)
+            # output = np.sqrt(dolfin.assemble(solv*solv*dx(4)))
         return output
+
+    problemfems = dict(mmat=mmat, cmat=cmat,
+                       bcinds=bcinds, bcvals=bcvals, ininds=ininds)
 
     return realize_linop, realize_sol, realize_output, problemfems
--- scripts/monte_carlo_sim.py
+++ scripts/monte_carlo_sim.py
@@ -6,12 +6,14 @@ import matplotlib.pyplot as plt
 
 import spacetime_galerkin_pod.chaos_expansion_utils as ceu
 import spacetime_galerkin_pod.ten_sor_utils as tsu
+import spacetime_galerkin_pod.gen_pod_utils as gpu
 
 from spacetime_galerkin_pod.ldfnp_ext_cholmod import SparseFactorMassmat
 
 from circle_subsec import get_problem
 
 get_linop, get_sol, get_output, problemfems = get_problem()
+print(problemfems['mmat'].shape[0])
 
 uncdims = 5
 
@@ -20,18 +22,17 @@ varia = 0.
 varib = 5e-4
 nua, nub = basenu+varia, basenu+varib
 
-mcits, mcruns = 10, 500  # 200
-ydim = 1  # dimension of the output
-pcedimlist = [2, 3]  # , 5]
-# pcedimlist = [3]  # , 5]
+mcits, mcruns = 20, 5000  # 200
+# pcedimlist = [2, 3, 5]
+pcedimlist = [5]  # , 7]
 
 mcplease = False
 pceplease = False
 plotplease = False
 # ## make it come true
 # mcplease = True
-# pceplease = True
-# plotplease = True
+pceplease = True
+plotplease = True
 
 # ## CHAP Monte Carlo
 if mcplease:
@@ -74,6 +75,7 @@ if mcplease:
 # ## CHAP Polynomial Chaos Expansion
 
 if pceplease:
+    ydim = 1  # dimension of the output
     for pcedim in pcedimlist:
         pceylist = []
         ypcedims = [ydim]
@@ -101,6 +103,7 @@ if pceplease:
 
         print('pcedim={0:2.0f}, exypce={1}'.
               format(pcedim, 1./((varib-varia)**uncdims)*exypce))
+        pceexy = 1./((varib-varia)**uncdims)*exypce
 
 # ## CHAP genpod
 pcedim = pcedimlist[0]
@@ -124,7 +127,80 @@ facmy = SparseFactorMassmat(mmat)
 pcemmat = sps.csc_matrix(sps.dia_matrix((weights, 0), shape=(pcedim, pcedim)))
 facmpce = SparseFactorMassmat(pcemmat)
 
-poddim = 5
-massfaclist = [facmy.L]
-massfaclist.extend([facmpce.L]*uncdims)
-ypodvecs = tsu.modeone_massmats_svd(yrslttns, massfaclist, kdim=poddim)
+basisfrom = 'mc'
+basisfrom = 'pce'
+poddimlist = [5, 10, 20, 40]
+nmcsnapshots = 5*pcedim**uncdims
+
+if basisfrom == 'pce':
+    pceymat = np.array(pceylist).T
+    pceymat = pceymat[0, :, :]
+elif basisfrom == 'mc':
+    varinu = basenu + (varib-varia)*np.random.rand(nmcsnapshots, uncdims)
+    # expvnu = np.average(varinu, axis=0)
+    # print('expected value of nu: ', expvnu)
+    varinulst = []
+    for uncdim in range(uncdims):
+        varinulst.append(varinu[:, uncdim].tolist())
+    ylist = []
+    for mck in range(nmcsnapshots):
+        for uncdim in range(uncdims):
+            nulist[uncdim] = varinulst[uncdim].pop(0)
+        cury = get_sol(nulist)
+        ylist.append(cury)
+    pceymat = np.array(ylist).T
+    pceymat = pceymat[0, :, :]
+
+lypceymat = facmy.Ft*pceymat
+# lypceymat = pceymat
+for poddim in poddimlist:
+    ypodvecs = gpu.get_ksvvecs(sol=lypceymat, poddim=poddim,
+                               plotsvs=plotplease, labl='Singular Values')
+
+    # massfaclist = [facmy.F]
+    # massfaclist.extend([facmpce.F]*uncdims)
+    # ypodvecs = tsu.modeone_massmats_svd(yrslttns, massfaclist, kdim=poddim)
+
+    lyitVy = facmy.solve_Ft(ypodvecs)
+    # lyitVy = ypodvecs
+
+    if plotplease:
+        yfull = get_output(nuarray.tolist(), plotfignum=222)
+        yred = get_output(nuarray.tolist(), plotfignum=111, podmat=lyitVy)
+
+    if pceplease:
+        ydim = 1  # dimension of the output
+        for pcedim in pcedimlist:
+            pceylist = []
+            ypcedims = [ydim]
+            ypcedims.extend([pcedim]*uncdims)
+            ypcedims = tuple(ypcedims)
+
+            abscissae, weights = ceu.get_gaussqr_uniform(N=pcedim,
+                                                         a=nua, b=nub)
+            # abscarray, weightsarray = np.array(abscissae), np.array(weights)
+
+            nulist = [basenu]*5
+            nuarray = np.array(nulist)
+            for idxtuple in itertools.product(np.arange(pcedim),
+                                              repeat=uncdims):
+                idxarray = np.array(idxtuple)
+                nuarray[:uncdims] = abscissae[idxarray]
+                pceylist.append(get_output(nuarray.tolist(), plotfignum=None,
+                                           podmat=lyitVy))
+
+            yrslttns = np.array(pceylist).reshape(ypcedims)
+            yrslttns = tsu.tnsrtrnsps(yrslttns)
+            exypce = 0
+            # for kk, cw in enumerate(weights):
+            for idxtuple in itertools.product(np.arange(pcedim),
+                                              repeat=uncdims):
+                idxarray = np.array(idxtuple)
+                cw = (weights[idxarray]).prod()
+                exypce += cw*yrslttns[idxtuple]
+
+            print('pcedim={0:2.0f}, poddim={2:2.0f}, exypce={1}'.
+                  format(pcedim, 1./((varib-varia)**uncdims)*exypce-pceexy,
+                         poddim))
+
+plt.show()
