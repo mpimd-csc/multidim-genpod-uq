7fcfd9743a3cec2901094d8cfc848745a26dd51a
Jan Heiland <jnhlbd@gmail.com>
2019-12-04 17:31:53 +0100

    some-sims

--- scripts/run_the_sims.py
+++ scripts/run_the_sims.py
@@ -1,15 +1,15 @@
 from mc_pce_gp import simit
 
 mcruns = 10000  # 200
-pcedimlist = [3]  # , 3, 4, 5]  # , 7]
+pcedimlist = [3, 5]  # , 3, 4, 5]  # , 7]
 mcplease = False
 pceplease = False
 plotplease = False
 mcpod = False
 pcepod = False
 # ## make it come true
-mcplease = True
-# pceplease = True
+# mcplease = True
+pceplease = True
 # plotplease = True
 # pcepod = True
 # mcpod = True
