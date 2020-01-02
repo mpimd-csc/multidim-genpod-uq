04d7026eda76932322e6cc19d5e582e58641e4f7
Jan Heiland <jnhlbd@gmail.com>
2020-01-02 15:57:11 +0100

    minor upds

--- scripts/sh-run.sh
+++ scripts/sh-run.sh
@@ -2,16 +2,17 @@ source addpypath.sh
 MESH=1
 MC=0
 PCE=0
-PCEPOD=0
-BASISFROM=mc
+PCEPOD=1
+BASISFROM=pce
 MCRUNS=10000
-PCESNAPDIM=3
+PCESNAPDIM=2
 NPCESNAP=$(($PCESNAPDIM**5))
 MCSNAP=$((5*$NPCESNAP))
-echo $MCSNAP
+LOGFILE=alldump
+echo 'tail -f logs/'$LOGFILE
 
 python3 run_the_sims.py --mesh $MESH --mc $MC --pce $PCE --podbase $BASISFROM \
-    --pcepod $PCEPOD --pcesnapdim $PCESNAPDIM --mcsnap $MCSNAP --mcruns $MCRUNS
-    >> logs/alldump
+    --pcepod $PCEPOD --pcesnapdim $PCESNAPDIM --mcsnap $MCSNAP --mcruns $MCRUNS \
+    >> logs/$LOGFILE
     # --mcpod 1 \
     # --mc 1000 \
