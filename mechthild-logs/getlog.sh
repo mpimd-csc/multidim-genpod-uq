7d9135c68d6398732c497ef9ee9391486d69c768
Jan Heiland <jnhlnd@gmail.com>
2020-08-28 13:09:46 +0200

    rmd things

--- mechthild-logs/getlog.sh
+++
@@ -1,3 +0,0 @@
-rsync --delete -avz mechthild.mpi-magdeburg.mpg.de:19-genpod-uq/scripts/job-files .
-rsync --delete -avz mechthild.mpi-magdeburg.mpg.de:19-genpod-uq/scripts/results .
-rsync --delete -avz mechthild.mpi-magdeburg.mpg.de:19-genpod-uq/scripts/*.json jsons/
