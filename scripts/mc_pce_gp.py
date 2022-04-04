68a9bf7b5724dad63e63a801ea2b8a0b6487b51a
Jan Heiland <jnhlnd@gmail.com>
2022-04-04 13:49:38 +0200

    debug info

--- scripts/mc_pce_gp.py
+++ scripts/mc_pce_gp.py
@@ -270,7 +270,8 @@ def simit(problem='circle', meshlevel=None,
         logging.info(f'truth: PCE({pcedimlist[-1]}): V(y): {truthvrnc}')
         logging.debug('loaded from ' + databasemoments)
     else:
-        logging.info('no truthvalues loaded')
+        logging.info('no truthvalues loaded -- tried with ' + databasemoments)
+        logging.debug(f'for with pcedim={pcedimlist[-1]}')
     tdict = dict(truthvrnc=truthvrnc, truthexpy=truthexpy)
 
     np.random.seed(1)  # seed for the random `mc` basis
