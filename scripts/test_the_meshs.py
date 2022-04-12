d3c73efb499e9164fcc90e223f348242fc46337b
Jan Heiland <jnhlnd@gmail.com>
2022-04-12 07:20:31 +0200

    now checking at all quad points

--- scripts/test_the_meshs.py
+++ scripts/test_the_meshs.py
@@ -10,17 +10,32 @@ problem = 'cylinder'
 plotplease = False
 meshlevellist = np.arange(5, 12)
 meshlevellist = np.arange(12, 13)
+meshlevellist = np.arange(4, 8)
 distribution = 'beta-2-5'
+multiproc = 4
+fullsweep = True
+
+simpars = dict(problem=problem, multiproc=multiproc,
+               distribution=distribution,
+               omtp_dict=dict(fullsweep=fullsweep, pcedim=2),
+               nulb=5e-4, nuub=1e-3, plotplease=plotplease, onlymeshtest=True)
 
 dofslist, ylist = [], []
 for meshlevel in meshlevellist:
-    dofs, outpt = simit(problem=problem, meshlevel=meshlevel,
-                        distribution=distribution,
-                        nulb=3e-4, nuub=7e-4,
-                        plotplease=plotplease, onlymeshtest=True)
+    simpars.update(dict(meshlevel=meshlevel))
+    if fullsweep:
+        dofs, outpt, absc = simit(**simpars)
+        ylist.append(outpt.reshape(-1))
+    else:
+        dofs, outpt = simit(**simpars)
     dofslist.append(dofs)
-    ylist.append(outpt)
 
-for k, ml in enumerate(meshlevellist):
-    b = [f'{x:.5f}' for x in ylist[k]]
-    print('Mesh:{0} | dofs:{1} | y:{2}'.format(ml, dofslist[k], b))
+if fullsweep:
+    np.set_printoptions(precision=4)
+    for kkk, ml in enumerate(meshlevellist[1:]):
+        valdiff = ylist[kkk+1] - ylist[kkk]
+        print(f'Mesh:{ml} | {valdiff}')
+else:
+    for k, ml in enumerate(meshlevellist):
+        b = [f'{x:.5f}' for x in ylist[k]]
+        print('Mesh:{0} | dofs:{1} | y:{2}'.format(ml, dofslist[k], b))
